71e3fb0f3800bfa33f512706c65fbe9a65dd6347
gautamtata <gautamtata@gmail.com>
2018-03-23 11:30:57 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,93 +1,61 @@
 <?php
 include 'dbConnection.php';
 $conn = getDatabaseConnection();
-function getDeviceTypes() {
+function getBankTypes() {
     global $conn;
     $sql = "SELECT DISTINCT(department)
             FROM `bankType`
             ORDER BY department";
-
     $stmt = $conn->prepare($sql);
     $stmt->execute();
     $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
     foreach ($records as $record) {
-
         echo "<option> "  . $record['bankType'] . "</option>";
-
     }
 }
 function displayBanks(){
     global $conn;
-
     $sql = "SELECT * FROM department WHERE 1";
-
     if (isset($_GET['submit'])){
-
         $namedParameters = array();
-
-
-
         if (!empty($_GET['departmentId'])) {
-
             //The following query allows SQL injection due to the single quotes
             //$sql .= " AND bankType LIKE '%" . $_GET['bankType'] . "%'";
-
             $sql .= " AND departmentId LIKE :departmentId"; //using named parameters
             $namedParameters[':bankType'] = "%" . $_GET['bankType'] . "%";
          }
-
-
          if (!empty($_GET['bankType'])) {
-
             //The following query allows SQL injection due to the single quotes
             //$sql .= " AND bankType LIKE '%" . $_GET['bankType'] . "%'";
-
             $sql .= " AND bankType = :dType"; //using named parameters
             $namedParameters[':dType'] =   $_GET['bankType'] ;
          }
-
          if (isset($_GET['available'])) {
              echo $_GET['status'];
              $sql .= " AND status = :status";
              $namedParameters[':status'] =  $_GET['available'];
-
          }
-
          if(isset($_GET['orderBy']) && $_GET['orderBy'] == 'name')     {
                   $sql .= " ORDER BY deptName";
         } else if(isset($_GET['orderBy']) && $_GET['orderBy'] == 'ID'){
                   $sql .= " ORDER BY departmetId";
         }
-
-
-
-
-
     }//endIf (isset)
-
       else  {
         $sql .= " ORDER BY bankType";
     }
-
-
      //echo "<br/>". $sql;
     //If user types a bankType
      //   "AND bankType LIKE '%$_GET['bankType']%'";
-    //if user selects device type
-      //  "AND deviceType = '$_GET['deviceType']";
-
-
+    //if user selects bank type
+      //  "AND bankType = '$_GET['bankType']";
     $stmt = $conn->prepare($sql);
     $stmt->execute($namedParameters);
     $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
      foreach ($records as $record) {
-
-        echo  "<p><b>Device Name: </b> ".$record['bankType'] . " <b>Device Type:</b> " . $record['deviceType'] . " <br/><b>Price:</b> $" .
-              $record['price'] .  "  <b>Status:</b> " . $record['status'] .
-             "</p><a target='checkoutHistory' href='checkoutHistory.php?deviceId=".$record['deviceId']."'> Checkout History </a><br />";
-
+        echo  "<p><b>bank Name: </b> ".$record['bankType'] . " <b>bank Type:</b> " . $record['departmetId'] . " <br/><b>Id:</b> $" .
+              $record['departmetId'] .  "  <b>Status:</b> " . $record['bankType'] .
+             "</p><a target='checkoutHistory' href='checkoutHistory.php?deviceId=".$record['bankType']."'> Checkout History </a><br />";
     }
 }
 ?>
@@ -111,7 +79,7 @@ function displayBanks(){
             Type:
             <select name="bankType">
                 <option value="">Select One</option>
-                <?=getDeviceTypes()?>
+                <?=getBankTypes()?>
             </select>
 
              <input type="checkbox" name="available" id="available" value="A">
