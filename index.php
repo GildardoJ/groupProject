7ed97d5e44c6ac22f640caed57bbd06f0f892327
John Economides <jeconomides@csumb.edu>
2018-03-26 15:23:17 +0000

    added deptment ID

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <?php
 include 'dbConnection.php';
-$conn = getDatabaseConnection();
+$conn = getDatabaseConnection('groupProject');
+
 function getBankTypes() {
     global $conn;
-    $sql = "SELECT DISTINCT(department)
-            FROM `bankType`
-            ORDER BY department";
+     $sql = "SELECT DISTINCT(bankType)
+                FROM `department`
+                ORDER by bankType";
+                
     $stmt = $conn->prepare($sql);
     $stmt->execute();
     $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -17,19 +19,25 @@ function displayBanks(){
     global $conn;
     $sql = "SELECT * FROM department WHERE 1";
     if (isset($_GET['submit'])){
-        $namedParameters = array();
-        if (!empty($_GET['departmentId'])) {
+         $namedParameters = array();
+         if (!empty($_GET['departmentId'])) {
+            $sql .= " AND departmentId = :deptId"; //using named parameters
+            $namedParameters[':deptId'] = "%" . $_GET['departmentId'] . "%";  
+         }
+         if (!empty($_GET['deptName'])) {
             //The following query allows SQL injection due to the single quotes
             //$sql .= " AND bankType LIKE '%" . $_GET['bankType'] . "%'";
-            $sql .= " AND departmentId LIKE :departmentId"; //using named parameters
-            $namedParameters[':bankType'] = "%" . $_GET['bankType'] . "%";
+            $sql .= " AND deptName LIKE :deptName"; //using named parameters
+            $namedParameters[':deptName'] = "%" . $_GET['deptName'] . "%";
          }
+    }
          if (!empty($_GET['bankType'])) {
             //The following query allows SQL injection due to the single quotes
             //$sql .= " AND bankType LIKE '%" . $_GET['bankType'] . "%'";
             $sql .= " AND bankType = :dType"; //using named parameters
             $namedParameters[':dType'] =   $_GET['bankType'] ;
          }
+         
          if (isset($_GET['available'])) {
              echo $_GET['status'];
              $sql .= " AND status = :status";
@@ -37,10 +45,11 @@ function displayBanks(){
          }
          if(isset($_GET['orderBy']) && $_GET['orderBy'] == 'name')     {
                   $sql .= " ORDER BY deptName";
-        } else if(isset($_GET['orderBy']) && $_GET['orderBy'] == 'ID'){
+        } 
+         else if(isset($_GET['orderBy']) && $_GET['orderBy'] == 'ID'){
                   $sql .= " ORDER BY departmetId";
         }
-    }//endIf (isset)
+    //endIf (isset)
       else  {
         $sql .= " ORDER BY bankType";
     }
@@ -53,9 +62,9 @@ function displayBanks(){
     $stmt->execute($namedParameters);
     $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
      foreach ($records as $record) {
-        echo  "<p><b>bank Name: </b> ".$record['bankType'] . " <b>bank Type:</b> " . $record['departmetId'] . " <br/><b>Id:</b> $" .
-              $record['departmetId'] .  "  <b>Status:</b> " . $record['bankType'] .
-             "</p><a target='checkoutHistory' href='checkoutHistory.php?deviceId=".$record['bankType']."'> Checkout History </a><br />";
+        echo  "<p><b>Bank Name: </b> ".$record['bankType'] . " <b>Dept Type:</b> " . $record['deptName'] . " <br/><b>Id:</b> " . $record['departmentId'] .  
+             // "  <b>Status:</b> " . $record['bankType'] .
+               "</p><a target='checkoutHistory' href='checkoutHistory.php?deviceId=".$record['bankType']."'> Checkout History </a><br />";
     }
 }
 ?>
@@ -66,7 +75,7 @@ function displayBanks(){
         <title>Team Project: Bank Search </title>
         <meta charset="utf-8">
         <style>
-               @import url('css/styles.css');
+               /*@import url('css/styles.css');*/
         </style>
     </head>
     <body>
@@ -75,11 +84,13 @@ function displayBanks(){
         </header>
 
         <form>
-             Device: <input type="text" name="bankType" placeholder="Bank Name"/>
+            Bank: <input type="text" name="bankType" placeholder="Bank Name"/>
             Type:
-            <select name="bankType">
+            <select name="bankType" id ="department">
                 <option value="">Select One</option>
-                <?=getBankTypes()?>
+                <?php
+                    getBankTypes()
+                ?>
             </select>
 
              <input type="checkbox" name="available" id="available" value="A">
@@ -88,20 +99,23 @@ function displayBanks(){
             <br>
             Order by:
             <input type="radio" name="orderBy" id="orderByName" value="name">
-            <label for="orderByName"> Name </label>
+            <label for="orderByName"> Bank Name </label>
 
-            <input type="radio" name="orderBy" id="orderByPrice" value="price">
-            <label for="orderByPrice"> Price </label>
+            <input type="radio" name="orderBy" id="orderByType" value="type">
+            <label for="orderByType"> Bank Type </label>
 
-
-
+            <input type="radio" name="orderBy" id="orderByUser" value="deptID">
+            <label for="orderByUser"> Departmant ID </label>
+            
             <input type="submit" value="Search!" name="submit" >
         </form>
 
 
         <hr>
         <main>
-        <?=displayBanks()?>
+        <?php
+        displayBanks()
+        ?>
         </main>
         <div id="frame">
          <iframe name="Banks" height="600" allowtransparency="true"></iframe>
