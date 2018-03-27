2d9fbed744f197b699a3610dbccc786341cb4c5a
Gildardo Orozco <gorozco@csumb.edu>
2018-03-27 03:39:45 +0000

    cart session variable added.

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <?php
+
+session_start();
 include 'dbConnection.php';
 $conn = getDatabaseConnection("groupProject");
 
 function getBankTypes() {
     global $conn;
-    $sql = "SELECT DISTINCT(bankType)
+    $sql = "SELECT bankType
             FROM `department`
             ORDER BY bankType";
     $stmt = $conn->prepare($sql);
@@ -57,16 +59,42 @@ function displayBanks(){
     $stmt = $conn->prepare($sql);
     $stmt->execute($namedParameters);
     $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    
      foreach ($records as $record) {
-        echo  "<p><b>bank Name: </b> ".$record['bankType'] . " <b>bank Type:</b> " . $record['departmetId'] . " <br/><b>Id:</b> $" .
-              $record['departmetId'] .  "  <b>Status:</b> " . $record['bankType'] .
-             "</p><a target='checkoutHistory' href='checkoutHistory.php?deviceId=".$record['bankType']."'> Checkout History </a><br />";
-    }
+        echo  "<p><b>bank Name: </b> ".$record['bankType'] . " <br/><b>Id:</b> " .
+              $record['departmentId'] .  "  <b>Status:</b> " . $record['bankType'] .
+             "</p><a target='' href='addCart.php?addCart=".$record['departmentId']."'> Add to cart </a><br />";
+            
+     }
+     
+   
+     
 }
+
+function showCart(){
+    global $conn;
+    $sql = "SELECT * FROM department WHERE 1";
+    
+    
+    if(isset($_SESSION['cart'])){
+         echo "<table class= 'table table-hover' border = '1'>
+                    <tr>
+                        <th>Member</th>
+                        <th>Account type</th>
+                    </tr>";
+        //var_dump($_SESSION['cart']);
+        foreach($_SESSION['cart'] as $elements){
+                echo "<tr>";
+                echo "<td>" . $elements , "</td>" ;
+                echo "<tr>";
+        }
+     }
+}
+
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="en">
     <head>
         <title>Team Project: Bank Search </title>
         <meta charset="utf-8">
@@ -78,44 +106,54 @@ function displayBanks(){
         <header>
         <h1>Banking Systems</h1>
         </header>
-
-        <form>
-             Bank: <input type="text" name="bankType" placeholder="Bank Name"/>
-            Type:
-            <select name="bankType">
-                <option value="">Select One</option>
-                
-                <?php
-                echo " before getBankTypes()";
-                getBankTypes();
-                echo " after getBankTypes()";
-                ?>
-            </select>
-
-             <input type="checkbox" name="available" id="available" value="A">
-            <label for="available"> Available </label>
-
-            <br>
-            Order by:
-            <input type="radio" name="orderBy" id="orderByName" value="name">
-            <label for="orderByName"> Name </label>
-
-            <input type="radio" name="orderBy" id="orderByPrice" value="price">
-            <label for="orderByPrice"> Price </label>
-
-
-
-            <input type="submit" value="Search!" name="submit" >
-        </form>
-
+            <div >
+            <form>
+                 Member: <input type="text" name="userId" placeholder="Member?"/>
+                Type:
+                <select name="bankType">
+                    <option value="">Select One</option>
+                    <?php
+                    getBankTypes();
+                    ?>
+                </select>
+    
+                <input type="checkbox" name="available" id="available" value="A">
+                <label for="available"> Available </label>
+    
+                <br>
+                Order by:
+                <input type="radio" name="orderBy" id="orderByName" value="name">
+                <label for="orderByName"> Name </label>
+    
+                <input type="radio" name="orderBy" id="orderByPrice" value="price">
+                <label for="orderByPrice"> Price </label>
+    
+    
+    
+                <input type="submit" value="Search!" name="submit" >
+            </form>
+            <form action = "reset.php" method = "post">
+                    Clear Cart <input type= "Submit">
+            </form>
+            
+            
+        </div>
+        <div>
+            
+        </div>
 
         <hr>
         <main>
         <?=displayBanks()?>
         </main>
-        <div id="frame">
-         <iframe name="Banks" height="600" allowtransparency="true"></iframe>
-         </div>
+        <div id="right">
+            <h2> Your Cart </h2>
+            <br>
+            <!-- <iframe name="Banks" height="600" allowtransparency="true"></iframe> -->
+             <?php
+                showCart();
+            ?>
+        </div>
 
     </body>
 </html>
